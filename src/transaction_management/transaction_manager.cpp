bfd35d9292747e506e21870d456f3b8947765fb7
Kulpreet Singh <zapfmann@gmail.com>
2019-09-18 17:53:40 +0200

    WIP - Messing with atomic counters
    Need a way to bulk assign transaction ids

--- src/transaction_management/transaction_manager.cpp
+++ src/transaction_management/transaction_manager.cpp
@@ -16,15 +16,41 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <bitcoin/database/transaction_management/transaction_context.hpp>
+
+#include <bitcoin/database/transaction_management/transaction_manager.hpp>
 
 
 namespace libbitcoin {
 namespace database {
 
-transaction_context::transaction_context() {
-    // txn_id = get next id using atomic;
-    // begin_timestamp = set to now;
+transaction_manager::transaction_manager(std::function<void()> callback)
+  : snapshot_callback_(callback),
+    quiescent(false)
+{
+    active_count_.store(0);
+}
+
+void transaction_manager::begin_transaction()
+{
+    active_count_.fetch_add(1, std::memory_order_relaxed);
+}
+
+void transaction_manager::end_transaction()
+{
+    if (active_count_.fetch_sub(1, std::memory_order_relaxed) == 1) {
+        if (quiescent)
+            snapshot_callback_();
+    }
+}
+
+void transaction_manager::enter_quiescent()
+{
+    quiescent = true;
+}
+
+void transaction_manager::leave_quiescent()
+{
+    quiescent = false;
 }
 
 }
